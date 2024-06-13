2d9ea7baa4901e7a06f48f8fde1fe7bf9354ec0a
Italo Barboza <italo_barboza@hotmail.com>
2024-06-13 11:48:17 -0300

    init create template

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  <RouterView />
+</template>
+
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
